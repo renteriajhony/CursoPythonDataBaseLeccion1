d94c6202b93b2bbad7652242a555fb63ee2a863c
Jhony Renteria Rodriguez <jhonyrenteria@gmail.com>
2024-12-11 14:42:36 -0500

    uso de with con psycopg2

--- prueba_bd.py
+++ prueba_bd.py
@@ -2,10 +2,15 @@ import psycopg2
 
 conexion = psycopg2.connect(user='postgres', password='postgres', host='localhost', port='5432', database='test_db')
 
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM PUBLIC.PERSON'
-cursor.execute(sentencia)
-resultado = cursor.fetchall()
-print(resultado)
-cursor.close()
-conexion.close()
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM PUBLIC.PERSON'
+            cursor.execute(sentencia)
+            resultado = cursor.fetchall()
+            print(resultado)
+            cursor.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    conexion.close()

de07087e86dfe0757c2b35168a25d895ddf1b825
Jhony Renteria Rodriguez <jhonyrenteria@gmail.com>
2024-12-11 15:02:51 -0500

    uso de fetch All con psycopg2

--- prueba_bd.py
+++ prueba_bd.py
@@ -18,11 +18,20 @@ try:
             # print(resultado)
 
             #Recuperamos solo un registro
-            sentencia = 'SELECT id_person, name, last_name, email FROM PUBLIC.PERSON WHERE id_person = %s'
-            my_value_var = int (input('Ingresa el id de la persona: '))
-            cursor.execute(sentencia, (my_value_var,))
-            resultado = cursor.fetchone()
-            print(resultado)
+            # sentencia = 'SELECT id_person, name, last_name, email FROM PUBLIC.PERSON WHERE id_person = %s'
+            # my_value_var = int (input('Ingresa el id de la persona: '))
+            # cursor.execute(sentencia, (my_value_var,))
+            # resultado = cursor.fetchone()
+            # print(resultado)
+
+            sentencia = 'SELECT id_person, name, last_name, email FROM PUBLIC.PERSON WHERE id_person IN %s'
+            # primary_keys = ((1,2,),)
+            entrada = input('Ingresa id \'s a buscar (separados por comas, Ej: 1,2,3): ')
+            primary_keys = (tuple(entrada.split(',')),)
+            cursor.execute(sentencia, primary_keys)
+            resultado = cursor.fetchall()
+            for registro in resultado:
+                print(registro)
 
 
 except (Exception, psycopg2.DatabaseError) as error:

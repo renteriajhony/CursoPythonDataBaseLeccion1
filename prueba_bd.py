304bc7f8532ae8d3306f6252311f4764c1f9f88a
Jhony Renteria Rodriguez <jhonyrenteria@gmail.com>
2024-12-11 14:53:02 -0500

    uso de fetch con psycopg2

--- prueba_bd.py
+++ prueba_bd.py
@@ -5,11 +5,26 @@ conexion = psycopg2.connect(user='postgres', password='postgres', host='localhos
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM PUBLIC.PERSON'
-            cursor.execute(sentencia)
-            resultado = cursor.fetchall()
+            # Recuperamos todos los valores de la tabla
+            # sentencia = 'SELECT * FROM PUBLIC.PERSON'
+            # cursor.execute(sentencia)
+            # resultado = cursor.fetchall()
+            # print(resultado)
+
+            #Recuperamos solo un registro
+            # sentencia = 'SELECT id_person, name, last_name, email FROM PUBLIC.PERSON WHERE id_person = 1'
+            # cursor.execute(sentencia)
+            # resultado = cursor.fetchone()
+            # print(resultado)
+
+            #Recuperamos solo un registro
+            sentencia = 'SELECT id_person, name, last_name, email FROM PUBLIC.PERSON WHERE id_person = %s'
+            my_value_var = int (input('Ingresa el id de la persona: '))
+            cursor.execute(sentencia, (my_value_var,))
+            resultado = cursor.fetchone()
             print(resultado)
-            cursor.close()
+
+
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 finally:
